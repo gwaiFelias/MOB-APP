ef2fac0635e4fb54dfee898aa2529ccfef68ef66
Gwyneth Felias <gwairenas@gmail.com>
2026-05-19 18:03:07 +0800

    Task 2: Update Home tab text

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -27,7 +27,7 @@ export default function TabTwoScreen() {
           style={{
             fontFamily: Fonts.rounded,
           }}>
-          Explore
+          Task 2: Explore tab updated!
         </ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
